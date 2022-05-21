69df85d58d09f9803b927876e27337023e1b68a8
Jean <chakib3500@gmail.com>
2022-05-21 21:15:57 +0200

    setup routing

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Pages from './pages/Pages';
+import Category from './components/Category';
 export default function App() {
   return (
     <div className="App">
+      <Category/>
       <Pages/>  
     </div>
     
--- src/js/pages/Pages.js
+++ src/js/pages/Pages.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Home from './home'
+import Cuisine from './Cuisine'
+import { Route,Routes,BrowserRouter } from 'react-router-dom'
 
 function Pages() {
   return (
     <div>
-        <Home/>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/cuisine' element={<Cuisine/>}/>
+          </Routes> 
+        </BrowserRouter>
     </div>
   )
 }
